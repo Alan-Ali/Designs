2fff2ec3f321d5416513272f39a878b440d0a825
Alan-Ali <alaan376@gmail.com>
2022-09-08 11:01:45 +0300

    test

---
+++ assets/utils.js
@@ -0,0 +1,140 @@
+
+const JreplaceClasses = (element, css_class, replacement) => {
+    // console.log()
+    if (Array.isArray(css_class)) {
+        if(Array.isArray(element)){
+            for(let j = 0; element.length; j++){
+                for (let i = 0; i < css_class.length; i++) {
+                    if (element[j].prop('classList').contains(css_class[i])) {
+                        element[j].prop('classList').remove(css_class[i]);
+                    }
+                }
+                if (Array.isArray(replacement)) {
+                    for (let i = 0; i < replacement.length; i++) {
+                        element[j].prop('classList').add(replacement[i]);
+                    }
+                } else {
+                    element[j].prop('classList').add(replacement);
+                }
+            }
+
+        }else{
+            for (let i = 0; i < css_class.length; i++) {
+                if (element.prop('classList').contains(css_class[i])) {
+                    element.prop('classList').remove(css_class[i]);
+                }
+            }
+            if (Array.isArray(replacement)) {
+                for (let i = 0; i < replacement.length; i++) {
+                    element.prop('classList').add(replacement[i]);
+                }
+            } else {
+                element.prop('classList').add(replacement);
+            }
+        }
+
+
+
+    } else {
+        if(Array.isArray(element)){
+            for(let i = 0; i < element.length;i++){
+                if (element[i].prop('classList').contains(css_class)) {
+                    element[i].prop('classList').remove(css_class);
+                    if (Array.isArray(replacement)) {
+                        for (let j = 0; j < replacement.length; j++) {
+                            element[i].prop('classList').add(replacement[j]);
+                        }
+                    } else {
+                        element[i].prop('classList').add(replacement);
+                    }
+
+                }
+            }
+        }else if (element.prop('classList').contains(css_class)) {
+            element.prop('classList').remove(css_class);
+            if (Array.isArray(replacement)) {
+                for (let i = 0; i < replacement.length; i++) {
+                    element.prop('classList').add(replacement[i]);
+                }
+            } else {
+                element.prop('classList').add(replacement);
+            }
+
+        }
+
+    }
+    // return element;
+}
+
+
+
+
+const replaceClasses = (element, css_class, replacement) => {
+    // console.log()
+    if (Array.isArray(css_class)) {
+        if(Array.isArray(element)){
+            for(let j = 0; element.length; j++){
+                for (let i = 0; i < css_class.length; i++) {
+                    if (element[j].classList.contains(css_class[i])) {
+                        element[j].classList.remove(css_class[i]);
+                    }
+                }
+                if (Array.isArray(replacement)) {
+                    for (let i = 0; i < replacement.length; i++) {
+                        element[j].classList.add(replacement[i]);
+                    }
+                } else {
+                    element[j].classList.add(replacement);
+                }
+            }
+
+        }else{
+            for (let i = 0; i < css_class.length; i++) {
+                if (element.classList.contains(css_class[i])) {
+                    element.classList.remove(css_class[i]);
+                }
+            }
+            if (Array.isArray(replacement)) {
+                for (let i = 0; i < replacement.length; i++) {
+                    element.classList.add(replacement[i]);
+                }
+            } else {
+                element.classList.add(replacement);
+            }
+        }
+
+
+
+    } else {
+        if(Array.isArray(element)){
+            for(let i = 0; i < element.length;i++){
+                if (element[i].classList.contains(css_class)) {
+                    element[i].classList.remove(css_class);
+                    if (Array.isArray(replacement)) {
+                        for (let j = 0; j < replacement.length; j++) {
+                            element[i].classList.add(replacement[j]);
+                        }
+                    } else {
+                        element[i].classList.add(replacement);
+                    }
+
+                }
+            }
+        }else if (element.classList.contains(css_class)) {
+            element.classList.remove(css_class);
+            if (Array.isArray(replacement)) {
+                for (let i = 0; i < replacement.length; i++) {
+                    element.classList.add(replacement[i]);
+                }
+            } else {
+                element.classList.add(replacement);
+            }
+
+        }
+
+    }
+    // return element;
+}
+
+
+
