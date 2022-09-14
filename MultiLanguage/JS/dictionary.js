a15670b8c39282b30c0a250d6e8bb672627a6952
Alan-Ali <alaan376@gmail.com>
2022-09-15 01:06:48 +0300

    15th september  1st commit

---
+++ MultiLanguage/JS/dictionary.js
@@ -0,0 +1,94 @@
+const langs = {
+    "en": {
+        "type": "ltr",
+        "dictionary": {
+            store:"store",
+            french_fry_restaurant:"French Fry Restaurant",
+            restaurant:"Restaurant",
+            view_all:"View All",
+            steak_food:"Steak Food",
+            rest_desc:"Restaurant Description",
+            area:"Area",
+            total_employee:"Total Employee",
+            opening_time:"Opening Time",
+            closing_time:"Closing Time",
+            place:"Place",
+            road:"Road",
+            mail:"Mail",
+            phone:"Phone",
+            food_menu:"Food Menu",
+            food_name:"Food Name",
+            total_item:"Total Item",
+            total_sale:"Total Sale",
+            pizza:"Pizza",
+            sushi:"Sushi",
+            food_menu:"Food Menu",
+            product_name:"Product Name",
+            import_day:"Import Day",
+            smart_chair:"Smart Chair",
+            printer:"Printer",
+        }
+    },
+    "ku":{
+        "type": "rtl",
+        "dictionary": {
+            store:"بازاڕ",
+            french_fry_restaurant:"فرێنچ فرای ڕێستۆرانت",
+            restaurant:"ڕێستۆرانت",
+            view_all:"پیشاندان",
+            steak_food:"ستیک و خواردنی خێرا",
+            rest_desc:"زانیاری ڕێستۆرانت",
+            area:"ناوچە",
+            total_employee:"بڕی کارمەندەکان",
+            opening_time:"کاتی کردنەوە",
+            closing_time:"کاتی داخستن",
+            place:"شوێن",
+            road:"ڕێگا",
+            mail:"مەیڵ",
+            phone:"تەلەفون",
+            food_menu:"مێنوی خواردن",
+            food_name:"ناوی خواردن",
+            total_item:"بڕی کەلوپەل",
+            total_sale:"بڕی فرۆشتن",
+            pizza:"پیتزا",
+            sushi:"سوشی",
+            food_menu:"مێنوی خواردن",
+            product_name:"ناوی پرۆدەکت",
+            import_day:"ڕۆژی ناردن",
+            smart_chair:"کورسی زیرەک",
+            printer:"پرینتەر",
+        }
+    },
+    // ar:{
+    //     type: "rtl",
+    //     dict: {
+    //         store:"store",
+    //         french_fry_restaurant:"French Fry Restaurant",
+    //         restaurant:"Restaurant",
+    //         view_all:"View All",
+    //         steak_food:"Steak Food",
+    //         rest_desc:"Restaurant Description",
+    //         area:"Area",
+    //         total_employee:"Total Employee",
+    //         opening_time:"Opening Time",
+    //         closing_time:"Closing Time",
+    //         food_menu:"Food Menu",
+    //         food_name:"Food Name",
+    //         total_item:"Total Item",
+    //         total_sale:"Total Sale",
+    //         pizza:"Pizza",
+    //         sushi:"Sushi",
+    //         food_menu:"Food Menu",
+    //         product_name:"Product Name",
+    //         import_day:"Import Day",
+    //         smart_chair:"Smart Chair",
+    //         printer:"Printer",
+    //     }
+    // }
+}
+
+
+if(!localStorage.getItem('langs')){
+    window.localStorage.setItem('langs', JSON.stringify(langs))
+}
+
---
+++ MultiLanguage/JS/multiLanguage.js
@@ -0,0 +1,150 @@
+const replaceClasses = (element, css_class, replacement) => {
+    // console.log()
+    if (Array.isArray(css_class)) {
+        if(Array.isArray(element)){
+            for(let j = 0; element.length; j++){
+                for (let i = 0; i < css_class.length; i++) {
+                    if (element[j].classList.contains(css_class[i])) {
+                        element[j].classList.remove(css_class[i]);
+                    }
+                }
+                if (Array.isArray(replacement)) {
+                    for (let i = 0; i < replacement.length; i++) {
+                        element[j].classList.add(replacement[i]);
+                    }
+                } else {
+                    element[j].classList.add(replacement);
+                }
+            }
+
+        }else{
+            for (let i = 0; i < css_class.length; i++) {
+                if (element.classList.contains(css_class[i])) {
+                    element.classList.remove(css_class[i]);
+                }
+            }
+            if (Array.isArray(replacement)) {
+                for (let i = 0; i < replacement.length; i++) {
+                    element.classList.add(replacement[i]);
+                }
+            } else {
+                element.classList.add(replacement);
+            }
+        }
+
+
+
+    } else {
+        if(Array.isArray(element)){
+            for(let i = 0; i < element.length;i++){
+                if (element[i].classList.contains(css_class)) {
+                    element[i].classList.remove(css_class);
+                    if (Array.isArray(replacement)) {
+                        for (let j = 0; j < replacement.length; j++) {
+                            element[i].classList.add(replacement[j]);
+                        }
+                    } else {
+                        element[i].classList.add(replacement);
+                    }
+
+                }
+            }
+        }else if (element.classList.contains(css_class)) {
+            element.classList.remove(css_class);
+            if (Array.isArray(replacement)) {
+                for (let i = 0; i < replacement.length; i++) {
+                    element.classList.add(replacement[i]);
+                }
+            } else {
+                element.classList.add(replacement);
+            }
+
+        }
+
+    }
+    // return element;
+}
+
+
+
+var byId = (id_name) => {
+    return document.getElementById(id_name)
+}
+var byClass = (class_name) => {
+    return document.getElementsByClassName(class_name)
+}
+let that = JSON.parse(localStorage.getItem('langs'));
+
+// console.log();
+
+
+
+const elem = document.querySelectorAll('.translate[data-dict]');
+console.log(that.ku['dictionary'])
+const class_cells = byClass('direction-cells');
+const class_words = byClass('direction-words');
+
+
+var translate = (lang) => {
+
+    var word = null; 
+    for(let i = 0; i < elem.length; i++) {
+        // console.log();
+        word = elem[i].getAttribute('data-dict')
+        elem[i].innerHTML = that[lang]['dictionary'][word];
+    }
+
+
+    for(let i=0; i< class_cells.length; i++) {
+        if(that[lang]['type'] === 'rtl'){
+            if(class_cells[i].classList.contains('flex-row')){
+                replaceClasses(class_cells[i], 'flex-row', 'flex-row-reverse');
+                replaceClasses(class_cells[i], 'direction-ltr', 'direction-rtl');
+            }
+            
+            // if(class_cells[i].classList.contains('justify-content-start')){
+            //     replaceClasses(class_cells[i], 'justify-content-start', 'justify-content-end');
+            // }
+
+            // if(class_cells[i].classList.contains('flex-column')){
+            //     replaceClasses(class_cells[i], 'flex-column', 'flex-column-reverse');
+            // }
+        }else{
+            if(class_cells[i].classList.contains('flex-row-reverse')){
+                replaceClasses(class_cells[i],'flex-row-reverse', 'flex-row' );
+                replaceClasses(class_cells[i], 'direction-rtl', 'direction-ltr');
+                // if(class_cells[i].classList.contains('justify-content-start')){
+                //     replaceClasses(class_cells[i], 'justify-content-start', 'justify-content-end');
+                // }
+            }
+            
+            // if(class_cells[i].classList.contains('flex-column-reverse')){
+            //     replaceClasses(class_cells[i], 'flex-column-reverse', 'flex-column');
+            // }            
+        }
+    }
+
+    for(let i=0; i< class_words.length; i++) {
+        if(that[lang]['type'] === 'rtl'){
+            replaceClasses(class_words[i], 'direction-ltr', 'direction-rtl');
+            
+        }else{
+            replaceClasses(class_words[i], 'direction-rtl', 'direction-ltr');
+            
+          
+        }
+    }
+    
+}
+
+const ku = byId('ku');
+const en = byId('en');
+
+
+ku.addEventListener('click', () => {
+    translate('ku');
+})
+en.addEventListener('click', () => {
+    translate('en');
+})
+
